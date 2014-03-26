fca8c9dbc7e4a10f2896b3dca0b349f3d07c1e81
lpageon <lpageon@figarocms.fr>
2014-03-26 14:32:57 +0100

    remove dependency to commons-io

--- src/main/java/org/groink/Main.java
+++ src/main/java/org/groink/Main.java
@@ -1,12 +1,10 @@
 package org.groink;
 
-import org.apache.commons.io.filefilter.FileFilterUtils;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        FileFilterUtils.falseFileFilter();
+        System.out.println("42");
 
     }
 

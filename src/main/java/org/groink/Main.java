6f6f1098534b4222229a5ea23b0c3178fd9783f4
lpageon <lpageon@figarocms.fr>
2014-03-26 14:17:28 +0100

    first commit

---
+++ src/main/java/org/groink/Main.java
@@ -0,0 +1,13 @@
+package org.groink;
+
+import org.apache.commons.io.filefilter.FileFilterUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FileFilterUtils.falseFileFilter();
+
+    }
+
+}
